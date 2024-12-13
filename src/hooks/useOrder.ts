7ef3437801519657c35f1b58c82d6666282c274e
kevinocotan <kaocotan@gmail.com>
2024-12-13 17:55:51 -0600

    evitando duplicados

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -7,13 +7,18 @@ export default function useOrder() {
 
     const addItem = (item: MenuItem) => {
 
-        const newItem = {...item, quantity: 1 }
-
-        setOrder([...order, newItem])
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if (itemExist) {
+            const updateOrder = order.map(orderItem => orderItem.id === item.id ? { ...orderItem, quantity: orderItem.quantity + 1 } : orderItem)
+            setOrder(updateOrder)
+        } else {
+            const newItem = { ...item, quantity: 1 }
+            setOrder([...order, newItem])
+        }
     }
 
     console.log(order);
-    
+
 
     return {
         addItem
