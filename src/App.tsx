0637038a1847557b28a1439233a38275e0ad5537
kevinocotan <kaocotan@gmail.com>
2024-12-13 15:58:24 -0600

    extensiones en react

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
 
 function App() {
@@ -12,6 +13,14 @@ function App() {
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
         <div>
           <h2>Menu</h2>
+
+          {menuItems.map(item => (
+            <MenuItem
+              key={item.id}
+              
+
+            />
+          ))}
         </div>
         <div>
           <h2>Consumo</h2>
