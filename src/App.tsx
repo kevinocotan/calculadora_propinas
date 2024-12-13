7abd646cae0b151ce7da083ce12f2e25174bbea0
kevinocotan <kaocotan@gmail.com>
2024-12-13 15:46:41 -0600

    agregando los productos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-
+import { menuItems } from "./data/db"
 
 function App() {
 
   return (
+    
     <>
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
+
+
     </>
   )
 }
