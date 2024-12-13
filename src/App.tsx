5afbc121efdc12433547a4f9aa8a305878c0dfc0
kevinocotan <kaocotan@gmail.com>
2024-12-13 15:52:34 -0600

    agregando los contenedores principales

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import { menuItems } from "./data/db"
 function App() {
 
   return (
-    
+
     <>
-      <header className="bg-teal-400 py-5">
+      <header className=" bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
 
-
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
