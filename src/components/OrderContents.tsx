72353b0234b0c5ce9c4b52f8f3f0b619f4eafeed
kevinocotan <kaocotan@gmail.com>
2024-12-14 19:11:41 -0600

    formatenado el consumo y mostrando cantidades

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,4 +1,5 @@
 import { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
 
 type OrderContentsProps = {
     order: OrderItem[]
@@ -8,18 +9,29 @@ export default function OrderContents({ order }: OrderContentsProps) {
     return (
         <div>
             <h2 className='font-black text-4xl'>Consumo</h2>
-            <div className="space-y-5 mt-5">
+            <div className="space-y-5 mt-10">
                 {order.length === 0 ?
                     <p className="text-center">La orden esta vacia.</p>
-                : (
-                    order.map(item => (
-                        <div key={item.id}>
-                            <p>
-                                {item.name}
-                            </p>
-                        </div>
-                    ))
-                )}
+                    : (
+                        order.map(item => (
+                            <div key={item.id}
+                                className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b">
+                                <div>
+                                    <p className="text=lg">
+                                        {item.name} - {formatCurrency(item.price)}
+                                    </p>
+                                    <p className="font-black">
+                                        Cantidad : {item.quantity} - {formatCurrency(item.price * item.quantity)}
+                                    </p>
+                                </div>
+
+                                <button
+                                    className="bg-red-600 h-8 w-8 rounded-full text-white font-black">
+                                    X
+                                </button>
+                            </div>
+                        ))
+                    )}
             </div>
         </div>
     )
